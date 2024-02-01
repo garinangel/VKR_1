2c1fd842129b6efe83238b90d0d4273d398a02f6
garinangel <146941191+garinangel@users.noreply.github.com>
2024-02-01 21:26:04 +0300

    Новые данные

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
-
-
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route('/', methods=["get","post"])
 def index():
-    return'Hello'
+
+    message =""
+    if request.method == "POST":
+        model = request.form.get("area")
+
+        message = f'Вычисленное значение'
+
+    return render_template('html.html', message=message)
 
 
 app.run()
+
+
+
